b5f3cb9aa1016b8c91688cf0ea1cdb56a0b13c3a
Issei Naruta <naruta@cookpad.com>
2020-07-17 23:13:58 +0900

    Update

--- webapp/frontend/javascript/TeamList.tsx
+++ webapp/frontend/javascript/TeamList.tsx
@@ -1,23 +1,23 @@
-import {xsuportal} from "./pb";
-import {ApiError, ApiClient} from "./ApiClient";
+import { xsuportal } from "./pb";
+import { ApiError, ApiClient } from "./ApiClient";
 import React from "react";
 
-import {ErrorMessage} from "./ErrorMessage";
+import { ErrorMessage } from "./ErrorMessage";
 
 export interface Props {
-  session: xsuportal.proto.services.common.GetCurrentSessionResponse,
-  client: ApiClient,
+  session: xsuportal.proto.services.common.GetCurrentSessionResponse;
+  client: ApiClient;
 }
 
 export interface State {
-  teamList: xsuportal.proto.services.audience.ListTeamsResponse | null,
-  error: Error | null,
+  teamList: xsuportal.proto.services.audience.ListTeamsResponse | null;
+  error: Error | null;
 }
 
 export class TeamList extends React.Component<Props, State> {
   constructor(props: Props) {
     super(props);
-    const params = new URLSearchParams(document.location.search)
+    const params = new URLSearchParams(document.location.search);
     this.state = {
       teamList: null,
       error: null,
@@ -31,21 +31,23 @@ export class TeamList extends React.Component<Props, State> {
   async updateTeamList() {
     try {
       const teamList = await this.props.client.listTeams();
-      this.setState({teamList});
+      this.setState({ teamList });
     } catch (error) {
-      this.setState({error});
+      this.setState({ error });
     }
   }
   public render() {
-    return <>
-      <header>
-        <h1 className="title is-1">参加チームリスト</h1>
-      </header>
-      <main>
-        {this.renderError()}
-        {this.renderTeamList()}
-      </main>
-    </>;
+    return (
+      <>
+        <header>
+          <h1 className="title is-1">参加チームリスト</h1>
+        </header>
+        <main>
+          {this.renderError()}
+          {this.renderTeamList()}
+        </main>
+      </>
+    );
   }
 
   public renderError() {
@@ -55,20 +57,33 @@ export class TeamList extends React.Component<Props, State> {
 
   renderTeamList() {
     if (!this.state.teamList) return <p>Loading...</p>;
-    return <>
-      {this.state.teamList.teams!.map((team,i) => this.renderTeam(team, i))}
-    </>;
+    return (
+      <>
+        {this.state.teamList.teams!.map((team, i) => this.renderTeam(team, i))}
+      </>
+    );
   }
 
-  renderTeam(team: xsuportal.proto.services.audience.ListTeamsResponse.ITeamListItem, i: number) {
-    return <div className="card mt-4" key={i}>
-      <div className="card-content">
-        <p className="title is-5">{team.name}</p>
-        {team.isStudent ? <p className="subtitle"><span className="tag is-info">学生チーム</span></p> : null}
-        <ul>
-          {team.memberNames!.map((name,j) => <li key={j}>{name}</li>)}
-        </ul>
+  renderTeam(
+    team: xsuportal.proto.services.audience.ListTeamsResponse.ITeamListItem,
+    i: number
+  ) {
+    return (
+      <div className="card mt-4" key={i}>
+        <div className="card-content">
+          <p className="title is-5">{team.name}</p>
+          {team.isStudent ? (
+            <p className="subtitle">
+              <span className="tag is-info">学生チーム</span>
+            </p>
+          ) : null}
+          <ul>
+            {team.memberNames!.map((name, j) => (
+              <li key={j}>{name}</li>
+            ))}
+          </ul>
+        </div>
       </div>
-    </div>;
+    );
   }
 }
