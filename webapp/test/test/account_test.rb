02a20c70446a693725046e83959441f8066241c5
Issei Naruta <naruta@cookpad.com>
2020-08-03 01:37:11 +0900

    Implement AccountTest

--- webapp/test/test/account_test.rb
+++ webapp/test/test/account_test.rb
@@ -1,15 +1,34 @@
 require_relative './test_base'
 
 class AccountTest < TestBase
-  test 'Sign up' do
+  test 'signup and login' do
     request_pb :post, '/initialize'
+
+    request_pb :post, '/api/login', {
+      contestant_id: 'mirakui',
+      password: 'pswd',
+    }
+    assert_equal '400', status
 
     request_pb :post, '/api/signup', {
       contestant_id: 'mirakui',
       password: 'pswd',
     }
+    assert_equal '200', status
 
-    assert_equal('200', status)
-    assert_equal({}, response)
+    request_pb :post, '/api/logout'
+    assert_equal '200', status
+
+    request_pb :post, '/api/login', {
+      contestant_id: 'mirakui',
+      password: 'badpswd',
+    }
+    assert_equal '400', status
+
+    request_pb :post, '/api/login', {
+      contestant_id: 'mirakui',
+      password: 'pswd',
+    }
+    assert_equal '200', status
   end
 end
