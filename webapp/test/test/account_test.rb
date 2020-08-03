7ebb2fd259b82354b6a1ad6dee5c81b76b88d7d3
Issei Naruta <naruta@cookpad.com>
2020-08-03 09:27:28 +0900

    Add ApiClient

--- webapp/test/test/account_test.rb
+++ webapp/test/test/account_test.rb
@@ -2,33 +2,33 @@ require_relative './test_base'
 
 class AccountTest < TestBase
   test 'signup and login' do
-    request_pb :post, '/initialize'
+    request :post, '/initialize'
 
-    request_pb :post, '/api/login', {
+    request :post, '/api/login', {
       contestant_id: 'mirakui',
       password: 'pswd',
     }
-    assert_equal '400', status
+    assert_equal 400, status
 
-    request_pb :post, '/api/signup', {
+    request :post, '/api/signup', {
       contestant_id: 'mirakui',
       password: 'pswd',
     }
-    assert_equal '200', status
+    assert_equal 200, status
 
-    request_pb :post, '/api/logout'
-    assert_equal '200', status
+    request :post, '/api/logout'
+    assert_equal 200, status
 
-    request_pb :post, '/api/login', {
+    request :post, '/api/login', {
       contestant_id: 'mirakui',
       password: 'badpswd',
     }
-    assert_equal '400', status
+    assert_equal 400, status
 
-    request_pb :post, '/api/login', {
+    request :post, '/api/login', {
       contestant_id: 'mirakui',
       password: 'pswd',
     }
-    assert_equal '200', status
+    assert_equal 200, status
   end
 end
--- webapp/test/test/test_base.rb
+++ webapp/test/test/test_base.rb
@@ -1,72 +1,42 @@
 require 'test/unit'
 require 'net/http'
-$LOAD_PATH << File.join(File.expand_path('../', __FILE__), 'lib')
-require 'routes'
+require_relative './api_client'
 
 class TestBase < Test::Unit::TestCase
   include Xsuportal::Routes
 
-  attr_reader :response, :status
+  class << self
+    def startup
+      @client = ApiClient.new
+    end
+
+    def shutdown
+    end
+
+    def client
+      @client
+    end
+  end
+
   def setup
-    @host = ENV.fetch('HOST', 'localhost')
-    @port = ENV.fetch('PORT', 9292)
-    @response = nil
-    @status = nil
-    @cookie = {}
   end
 
-  def http_get(path, headers={})
-    request = Net::HTTP::Get.new(path)
-    unless headers.empty?
-      headers.each do |key, value|
-        request[key] = value
-      end
-    end
-    Net::HTTP.start(@host, @port) do |http|
-      http.request(request)
-    end
+  def teardown
   end
 
-  def request_pb(method, path, payload={})
-    route = "#{method.upcase} #{path}"
-    request_class_pb, response_class_pb = PB_TABLE[route]
-    req = nil
-    case method
-    when :get
-      req = Net::HTTP::Get.new(path)
-    when :post
-      req = Net::HTTP::Post.new(path)
-    when :put
-      req = Net::HTTP::Put.new(path)
-    when :delete
-      req = Net::HTTP::Delete.new(path)
-    else
-      raise "Unsupported HTTP method: #{method}"
-    end
+  def client
+    self.class.client
+  end
 
-    if request_class_pb
-      req['Content-Type'] = 'application/vnd.google.protobuf'
-      req.body = request_class_pb.encode(request_class_pb.new(payload))
-    end
-    req['Accept'] = 'application/vnd.google.protobuf, text/plain'
-    req['Cookie'] = @cookie.map {|k,v| "#{k}=#{v}"}.join('; ')
+  def response
+    client.response
+  end
 
-    res = Net::HTTP.start(@host, @port) do |http|
-      http.request(req)
-    end
+  def status
+    client.status
+  end
 
-    if res['Set-Cookie']
-      cookie_key, cookie_value = res['Set-Cookie'].split(';')[0].split('=')
-      @cookie[cookie_key] = cookie_value
-    end
-
-    case res['Content-Type']
-    when /^application\/vnd\.google\.protobuf;?/
-      @response = response_class_pb.decode(res.body).to_h
-    else
-      @response = res.body
-    end
-    @status = res.code
-    @response
+  def request(method, path, payload={})
+    client.request(method, path, payload)
   end
 end
