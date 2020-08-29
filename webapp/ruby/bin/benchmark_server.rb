31c089df1ac53211efac519986a44e91f96ea470
Issei Naruta <naruta@cookpad.com>
2020-08-29 22:19:22 +0900

    Add LoggingInterceptor

--- webapp/ruby/bin/benchmark_server.rb
+++ webapp/ruby/bin/benchmark_server.rb
@@ -3,6 +3,8 @@
 $: << File.expand_path('../lib', __dir__)
 require 'logger'
 require 'griffin'
+require 'grpc'
+require 'griffin/interceptors/server/logging_interceptor'
 require_relative '../grpc/benchmark_queue_service'
 require_relative '../grpc/benchmark_report_service'
 require 'database'
@@ -17,7 +19,13 @@ Griffin::Server.configure do |c|
     BenchmarkReportService,
   ]
 
+  c.interceptors [
+    Griffin::Interceptors::Server::LoggingInterceptor.new,
+  ]
+
   c.workers 2
+  c.pool_size 10, 10
+  c.connection_size 10, 10
 
   c.logger Logger.new($stdout)
 end
