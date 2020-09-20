5a4737990f4f8c89965ddd89b8ad1dc927c3cea9
Issei Naruta <naruta@cookpad.com>
2020-09-20 14:03:30 +0900

    Remove pem and add generate_vapid_key.sh instead

---
+++ webapp/generate_vapid_key.sh
@@ -0,0 +1,9 @@
+#!/bin/sh -e
+DST_PATH=vapid_private.pem
+if [ -f $DST_PATH ]; then
+  echo "${DST_PATH} already exists"
+  exit 1
+fi
+openssl ecparam -name prime256v1 -genkey -noout -out $DST_PATH
+echo "Created: ${DST_PATH}"
+
