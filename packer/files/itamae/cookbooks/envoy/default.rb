2cfe0716f9bd1ec3f7e5e635f78073c9086b619a
Sorah Fukumori <sorah@cookpad.com>
2020-09-23 03:41:09 +0900

    packer: envoy

--- packer/files/itamae/cookbooks/envoy/default.rb
+++ packer/files/itamae/cookbooks/envoy/default.rb
@@ -1,3 +1,22 @@
+node.reverse_merge!(
+  envoy: {
+    slice: nil,
+  },
+)
+
 include_cookbook 'apt-source-getenvoy'
 
 package 'getenvoy-envoy'
+
+directory '/etc/envoy' do
+  owner 'root'
+  group 'root'
+  mode  '0755'
+end
+
+template '/etc/systemd/system/envoy.service' do
+  owner 'root'
+  group 'root'
+  mode  '0644'
+  notifies :run, 'execute[systemctl daemon-reload]'
+end
