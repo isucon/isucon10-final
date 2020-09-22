381ca8f6b3f551f57e22fbb3c98fe8f4d599c8d6
Sorah Fukumori <sorah@cookpad.com>
2020-09-23 03:41:09 +0900

    packer: envoy

--- packer/files/itamae/cookbooks/envoy/default.rb
+++ packer/files/itamae/cookbooks/envoy/default.rb
@@ -1,3 +1,16 @@
+node.reverse_merge!(
+  envoy: {
+    slice: nil,
+  },
+)
+
 include_cookbook 'apt-source-getenvoy'
 
 package 'getenvoy-envoy'
+
+template '/etc/systemd/system/envoy.service' do
+  owner 'root'
+  group 'root'
+  mode  '0644'
+  notifies :run, 'execute[systemctl daemon-reload]'
+end
--- packer/files/itamae/cookbooks/xsuportal/web.rb
+++ packer/files/itamae/cookbooks/xsuportal/web.rb
@@ -1,1 +1,21 @@
 include_cookbook 'envoy'
+
+directory '/etc/ssl/private' do
+  owner 'root'
+  group 'root'
+  mode  '0755'
+end
+
+execute 'install -m644 -o isucon -g isucon /dev/shm/files/tls-cert.pem /etc/ssl/private/tls-cert.pem' do
+  not_if 'test -e /etc/ssl/private/tls-cert.pem'
+end
+
+execute 'install -m600 -o isucon -g isucon /dev/shm/files/tls-key.pem /etc/ssl/private/tls-key.pem' do
+  not_if 'test -e /etc/ssl/private/tls-key.pem'
+end
+
+remote_file '/etc/envoy/config.yaml' do
+  owner 'root'
+  group 'root'
+  mode  '0644'
+end
