4ef7e6652c07b761d0b50f3aea9b0b6c0d95d3ae
Sorah Fukumori <sorah@cookpad.com>
2020-09-23 15:42:11 +0900

    packer: build webapp/rust

--- packer/files/itamae/cookbooks/xsuportal/rust.rb
+++ packer/files/itamae/cookbooks/xsuportal/rust.rb
@@ -1,1 +1,17 @@
 include_cookbook 'langs::rust'
+
+execute 'cd ~isucon/webapp/rust && /home/isucon/.x cargo build --release --locked'
+
+template '/etc/systemd/system/xsuportal-web-rust.service' do
+  owner 'root'
+  group 'root'
+  mode  '0644'
+  notifies :run, 'execute[systemctl daemon-reload]'
+end
+
+template '/etc/systemd/system/xsuportal-api-rust.service' do
+  owner 'root'
+  group 'root'
+  mode  '0644'
+  notifies :run, 'execute[systemctl daemon-reload]'
+end
