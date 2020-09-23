fda0a74e94d04cb6bec3ea5228eb47814a06674c
Sorah Fukumori <sorah@cookpad.com>
2020-09-24 02:20:33 +0900

    fix

--- packer/scripts/latest.rb
+++ packer/scripts/latest.rb
@@ -4,6 +4,8 @@ require 'aws-sdk-s3'
 BUCKET = 'isucon10-machine-images'
 PREFIX = 'final/'
 
+do_presign = ARGV.delete('--presign')
+
 s3 = Aws::S3::Client.new(use_dualstack_endpoint: true)
 
 manifest_obj = s3.list_objects_v2(bucket: BUCKET, prefix: "#{PREFIX}#{ARGV[0]}/")
@@ -17,7 +19,7 @@ qcow2_key = manifest.fetch('qcow2_key')
 qcow2_sha256 = manifest.fetch('qcow2_sha256')
 
 url = Aws::S3::Presigner.new(client: s3).presigned_url(:get_object, bucket: BUCKET, key: qcow2_key)
-if ARGV.delete('--presign')
+if do_presign
   puts({url: url, sum: qcow2_sha256}.to_json)
 else
   File.write "tmp/latest-#{ARGV[0]}.sum", "#{qcow2_sha256}\n"
