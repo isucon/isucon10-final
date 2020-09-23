305074f6c7ad3edf56b5f155f6682ee1469fdff5
Sorah Fukumori <sorah@cookpad.com>
2020-09-23 14:19:13 +0900

    packer(frontend): run as isucon

--- packer/files/itamae/cookbooks/xsuportal/frontend.rb
+++ packer/files/itamae/cookbooks/xsuportal/frontend.rb
@@ -1,5 +1,8 @@
-execute 'cd ~isucon/webapp/frontend && /home/isucon/.x yarn'
+execute 'cd ~isucon/webapp/frontend && /home/isucon/.x yarn' do
+  user 'isucon'
+end
 
 execute 'cd ~isucon/webapp/frontend && rm -rf public; NODE_ENV=production /home/isucon/.x npx webpack --mode production' do
+  user 'isucon'
   not_if 'test -e ~isucon/webapp/frontend/public'
 end
