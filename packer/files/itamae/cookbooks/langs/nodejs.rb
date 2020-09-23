2ec481731193fed692fe28f7f8c9cc684c93e6d8
Sorah Fukumori <sorah@cookpad.com>
2020-09-23 11:25:20 +0900

    packer: install yarn

--- packer/files/itamae/cookbooks/langs/nodejs.rb
+++ packer/files/itamae/cookbooks/langs/nodejs.rb
@@ -6,3 +6,8 @@ execute "rm -rf /home/isucon/local/nodejs; /opt/xbuild/node-install #{version} /
   user 'isucon'
   not_if "/home/isucon/local/nodejs/bin/node --version | grep -q '^#{version}$'"
 end
+
+execute 'curl -o- -L https://yarnpkg.com/install.sh | bash' do
+  user 'isucon'
+  not_if "test -e /home/isucon/.yarn"
+end
