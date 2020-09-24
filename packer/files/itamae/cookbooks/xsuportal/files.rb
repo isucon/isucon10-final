ed172b48b2978cf5aadcba95b7f139de3c88259e
Sorah Fukumori <sorah@cookpad.com>
2020-09-25 01:22:44 +0900

    set BENCHMARK_SERVER_{HOST,PORT} on boot using /etc/hosts

--- packer/files/itamae/cookbooks/xsuportal/files.rb
+++ packer/files/itamae/cookbooks/xsuportal/files.rb
@@ -1,3 +1,5 @@
+include_cookbook 'langs::ruby'
+
 execute "rm -rf ~isucon/webapp; tar xf /dev/shm/files-generated/archive.tar -C ~isucon/ webapp" do
   user "isucon"
   not_if "test -e ~isucon/webapp"
@@ -13,6 +15,24 @@ template "/home/isucon/env" do
   group 'root'
   mode  '0640'
 end
+
+remote_file "/opt/isucon-env-ensure-benchmark-server" do
+  owner 'root'
+  owner 'root'
+  mode  '0755'
+end
+
+remote_file "/etc/systemd/system/isucon-env-ensure-benchmark-server.service" do
+  owner 'root'
+  owner 'root'
+  mode  '0644'
+  notifies :run, 'execute[systemctl daemon-reload]', :immediately
+end
+
+service 'isucon-env-ensure-benchmark-server.service' do
+  action [:enable]
+end
+
 
 if node.dig(:xsuportal, :ci_cache)
   execute "cd /opt/ci-cache && rm /opt/ci-cache/.do && for x in *; do mkdir -p ~isucon/webapp/${x}; mv -v ${x}/* -t ~isucon/webapp/${x}/; done" do
