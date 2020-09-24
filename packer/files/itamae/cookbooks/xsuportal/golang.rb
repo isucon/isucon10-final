ee9f2223a37036a6c0457a09df2839b6eb691d82
Sorah Fukumori <sorah@cookpad.com>
2020-09-24 17:02:44 +0900

    packer(xsuportal::golang): Build

--- packer/files/itamae/cookbooks/xsuportal/golang.rb
+++ packer/files/itamae/cookbooks/xsuportal/golang.rb
@@ -1,1 +1,19 @@
 include_cookbook 'langs::golang'
+
+execute 'cd ~isucon/webapp/golang && /home/isucon/.x make' do
+  user 'isucon'
+end
+
+template '/etc/systemd/system/xsuportal-web-golang.service' do
+  owner 'root'
+  group 'root'
+  mode  '0644'
+  notifies :run, 'execute[systemctl daemon-reload]'
+end
+
+template '/etc/systemd/system/xsuportal-api-golang.service' do
+  owner 'root'
+  group 'root'
+  mode  '0644'
+  notifies :run, 'execute[systemctl daemon-reload]'
+end
