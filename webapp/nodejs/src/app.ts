df4ce8f2ede2cace6a854289e94e7f4b16d49daa
yosuke-furukawa <yosuke.furukawa@gmail.com>
2020-09-25 17:01:36 +0900

    wip

--- webapp/nodejs/src/app.ts
+++ webapp/nodejs/src/app.ts
@@ -13,7 +13,7 @@ const ADMIN_PASSWORD = 'admin'
 const DEBUG_CONTEST_STATUS_FILE_PATH = '/tmp/XSUPORTAL_CONTEST_STATUS'
 const hash = crypto.createHash('sha256');
 
-const db = await mysql.createConnection({
+const connection = mysql.createConnection({
   host: process.env['MYSQL_HOSTNAME'] ?? '127.0.0.1',
   port: Number.parseInt(process.env['MYSQL_PORT'] ?? '3306'),
   user: process.env['MYSQL_USER'] ?? 'isucon',
@@ -39,6 +39,7 @@ app.use(function(req, res, next) {
 });
 
 app.post("/initialize", async (req, res, next) => {
+  const db = await connection;
   const request = InitializeRequest.deserializeBinary(Buffer.from(req.body));
   await db.query('TRUNCATE `teams`');
   await db.query('TRUNCATE `contestants`')
@@ -56,16 +57,49 @@ app.post("/initialize", async (req, res, next) => {
   const contest = request.getContest();
   if (contest) {
     const openAt = contest.getRegistrationOpenAt();
-    if (openAt == null) {
-      // TODO error handle
-      return new Error().setCode(400);
+    const startsAt = contest.getContestStartsAt();
+    const freezeAt = contest.getContestFreezesAt();
+    const endsAt = contest.getContestFreezesAt();
+
+    if (openAt == null || startsAt == null || freezeAt == null || endsAt == null) {
+      res.status(400);
+      const error = new Error();
+      error.setCode(400);
+      error.setHumanMessage("contest date is invalid");
+      return res.send(error);
     }
+    
     await db.query(`INSERT contest_config (registration_open_at, contest_starts_at, contest_freezes_at, contest_ends_at) VALUES (?, ?, ?, ?)`, [
       new Date(openAt.getSeconds()).toUTCString(),
+      new Date(startsAt.getSeconds()).toUTCString(),
+      new Date(freezeAt.getSeconds()).toUTCString(),
+      new Date(endsAt.getSeconds()).toUTCString(),
     ]);
   } else {
-    // TODO
-
+    await db.query(`
+    INSERT contest_config (registration_open_at, contest_starts_at, contest_freezes_at, contest_ends_at) VALUES 
+    (
+      TIMESTAMPADD(SECOND, 0, NOW(6)),
+      TIMESTAMPADD(SECOND, 5, NOW(6)),
+      TIMESTAMPADD(SECOND, 40, NOW(6)),
+      TIMESTAMPADD(SECOND, 50, NOW(6))
+    )
+    `);
   }
+
+  // TODO 負荷レベルの指定
+  const response = new InitializeResponse();
+  response.setLanguage("nodejs");
+  // 実ベンチマーカーに伝える仮想ベンチマークサーバー(gRPC)のホスト情報
+  const benchmarkServer = new InitializeResponse.BenchmarkServer();
+  benchmarkServer.setHost("localhost");
+  benchmarkServer.setPort(50051);
+  response.setBenchmarkServer(benchmarkServer);
+
+  res.contentType(`application/vnd.google.protobuf; proto=xsuportal.services.admin.initialize`);
+  res.end(response.serializeBinary());
 });
 
+app.listen(9292, () => {
+  console.log("Listening on 9292");
+});
