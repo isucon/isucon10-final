711d782b99938c82981c8b532f69247fa9dc11d9
yosuke-furukawa <yosuke.furukawa@gmail.com>
2020-09-25 18:43:37 +0900

    add /api/admin/clarification

--- webapp/nodejs/src/app.ts
+++ webapp/nodejs/src/app.ts
@@ -1,10 +1,11 @@
 import express from "express";
-import * as grpc from "grpc";
 import mysql from "promise-mysql";
 import crypto from 'crypto';
 
 import {InitializeRequest, InitializeResponse} from "./proto/xsuportal/services/admin/initialize_pb";
-import {Error} from "./proto/xsuportal/error_pb";
+import {Error as PbError} from "./proto/xsuportal/error_pb";
+import { Clarification } from "./proto/xsuportal/resources/clarification_pb";
+import { ListClarificationsResponse } from "./proto/xsuportal/services/admin/clarifications_pb";
 
 const TEAM_CAPACITY = 10
 const MYSQL_ER_DUP_ENTRY = 1062
@@ -21,6 +22,25 @@ const connection = mysql.createConnection({
   password: process.env['MYSQL_PASS'] || 'isucon',
   charset: 'utf8mb4',
 });
+
+const haltPb = (res: express.Response, code: number, humanMessage: string) => {
+  res.contentType('application/vnd.google.protobuf; proto=xsuportal.proto.Error');
+  res.status(code);
+  const error = new PbError();
+  error.setCode(code);
+  error.setHumanMessage(humanMessage);
+  res.end(Buffer.from(error.serializeBinary()));
+};
+
+const haltPbWithError = (res: express.Response, code: number, humanMessage: string, err: Error) => {
+  res.contentType('application/vnd.google.protobuf; proto=xsuportal.proto.Error');
+  res.status(code);
+  const error = new PbError();
+  error.setCode(code);
+  error.setHumanMessage(humanMessage);
+  error.setHumanDescriptionsList([err.stack ?? err.message]);
+  res.end(Buffer.from(error.serializeBinary()));
+};
 
 const app = express();
 
@@ -62,11 +82,7 @@ app.post("/initialize", async (req, res, next) => {
     const endsAt = contest.getContestFreezesAt();
 
     if (openAt == null || startsAt == null || freezeAt == null || endsAt == null) {
-      res.status(400);
-      const error = new Error();
-      error.setCode(400);
-      error.setHumanMessage("contest date is invalid");
-      return res.send(error);
+      return haltPb(res, 400, "initialize の時間が不正です。");
     }
     
     await db.query(`INSERT contest_config (registration_open_at, contest_starts_at, contest_freezes_at, contest_ends_at) VALUES (?, ?, ?, ?)`, [
@@ -100,6 +116,26 @@ app.post("/initialize", async (req, res, next) => {
   res.end(Buffer.from(response.serializeBinary()));
 });
 
-app.listen(9292, () => {
+app.get("/api/admin/clarification", async (req, res, next) => {
+  const db = await connection;
+  // TODO login required
+  const clars = await db.query('SELECT * FROM `clarifications` ORDER BY `updated_at` DESC');
+
+  const clarPbs = [];
+  for (const clar of clars) {
+    const team = await db.query('SELECT * FROM `teams` WHERE `id` = ? LIMIT 1', [clar.team_id]);
+    const clarPb = new Clarification();
+    clarPb.setTeam(team);
+    clarPbs.push(clarPb);
+  }
+  
+  const response = new ListClarificationsResponse();
+  response.setClarificationsList(clarPbs);
+  res.contentType(`application/vnd.google.protobuf`);
+  res.end(Buffer.from(response.serializeBinary()));
+});
+
+
+app.listen(process.env.PORT ?? 9292, () => {
   console.log("Listening on 9292");
 });
