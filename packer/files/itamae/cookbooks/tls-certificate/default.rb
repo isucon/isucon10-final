8308470458b615c6f6a676176674b862a2a2387b
Sorah Fukumori <sorah@cookpad.com>
2020-09-26 03:41:32 +0900

    packer(benchmarker): Configure push service

---
+++ packer/files/itamae/cookbooks/tls-certificate/default.rb
@@ -0,0 +1,9 @@
+execute 'install -m644 -o isucon -g isucon /dev/shm/files/tls-cert.pem /etc/ssl/private/tls-cert.pem' do
+  not_if 'test -e /etc/ssl/private/tls-cert.pem'
+end
+
+execute 'install -m600 -o isucon -g isucon /dev/shm/files/tls-key.pem /etc/ssl/private/tls-key.pem' do
+  not_if 'test -e /etc/ssl/private/tls-key.pem'
+end
+
+
--- packer/files/itamae/roles/ci/files/home/isucon/ci.sh
+++ packer/files/itamae/roles/ci/files/home/isucon/ci.sh
@@ -13,10 +13,24 @@ sudo systemctl start xsuportal-api-${lang}.service
 
 sleep 5
 
-(
-  ~isucon/benchmarker/bin/benchmarker \
+sudo systemd-run \
+  --same-dir \
+  --pipe \
+  --wait \
+  --collect \
+  --uid=$(id -u)\
+  --gid=$(id -g) \
+  --slice=benchmarker.slice \
+  --service-type=oneshot \
+  -p AmbientCapabilities=CAP_NET_BIND_SERVICE \
+  -p CapabilityBoundingSet=CAP_NET_BIND_SERVICE \
+  -p LimitNOFILE=2000000 \
+  -p TimeoutStartSec=110s \
+    ~isucon/benchmarker/bin/benchmarker \
     -exit-status \
     -tls \
-    -target "local.t.isucon.dev:443" \
-    -host-advertise "localbench.t.isucon.dev"
-) 2>&1 | tee ~isucon/ci.log
+    -host-advertise localbench.t.isucon.dev \
+    -push-service-port 443 \
+    -tls-cert /etc/ssl/private/tls-cert.pem \
+    -tls-key /etc/ssl/private/tls-key.pem \
+  2>&1 | tee ~isucon/ci.log
