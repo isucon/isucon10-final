f35792a768f32e7db2187d24072e9add30e2bba3
yosuke-furukawa <yosuke.furukawa@gmail.com>
2020-09-25 17:02:05 +0900

    switch identified by for mysql client issue...

--- webapp/sql/setup.sql
+++ webapp/sql/setup.sql
@@ -1,3 +1,3 @@
 CREATE DATABASE IF NOT EXISTS `xsuportal` DEFAULT CHARACTER SET utf8mb4;
-CREATE USER IF NOT EXISTS `isucon`@`localhost` IDENTIFIED BY 'isucon';
+CREATE USER IF NOT EXISTS `isucon`@`localhost` IDENTIFIED WITH mysql_native_password BY 'isucon';
 GRANT ALL ON `xsuportal`.* TO `isucon`@`localhost`;
