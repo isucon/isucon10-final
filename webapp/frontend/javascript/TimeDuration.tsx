8dc825be3fb8b05b7f9f8d7017908448a0740472
Sorah Fukumori <sorah@cookpad.com>
2020-09-28 04:59:14 +0900

    frontend: Remove unneccessary dependencies

--- webapp/frontend/javascript/TimeDuration.tsx
+++ webapp/frontend/javascript/TimeDuration.tsx
@@ -1,5 +1,10 @@
-import { google } from "./pb";
-import moment from "moment";
+import type { google } from "./pb";
+
+import dayjs from "dayjs";
+import durationPlugin from "dayjs/plugin/duration";
+import relativeTimePlugin from "dayjs/plugin/relativeTime";
+dayjs.extend(durationPlugin);
+dayjs.extend(relativeTimePlugin);
 
 import React from "react";
 
@@ -9,14 +14,9 @@ export interface Props {
 }
 
 export const TimeDuration: React.FC<Props> = (props: Props) => {
-  const tA = moment(
-    (props.a.seconds as number) * 1000 + (props.a.nanos as number) / 1000000
-  );
-  const tB = props.b
-    ? moment(
-        (props.b.seconds as number) * 1000 + (props.b.nanos as number) / 1000000
-      )
-    : moment();
-  const d = moment.duration(tB.diff(tA));
-  return <span>{d.humanize({ s: 600, m: 120 })}</span>;
+  const tA = dayjs((props.a.seconds as number) * 1000 + (props.a.nanos as number) / 1000000);
+  const tB = props.b ? dayjs((props.b.seconds as number) * 1000 + (props.b.nanos as number) / 1000000) : dayjs();
+  const d = dayjs.duration(tB.diff(tA));
+  return <span>{d.humanize(false)}</span>;
 };
+
