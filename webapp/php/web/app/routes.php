3b72efa889f9d72781ec5d917f61801141041cdc
tamakiii <tamakiii@users.noreply.github.com>
2020-09-27 16:51:28 +0900

    init nginx

--- webapp/php/web/app/routes.php
+++ webapp/php/web/app/routes.php
@@ -1,12 +1,9 @@
 <?php
 declare(strict_types=1);
 
-use App\Application\Actions\User\ListUsersAction;
-use App\Application\Actions\User\ViewUserAction;
 use Psr\Http\Message\ResponseInterface as Response;
 use Psr\Http\Message\ServerRequestInterface as Request;
 use Slim\App;
-use Slim\Interfaces\RouteCollectorProxyInterface as Group;
 
 return function (App $app) {
     $app->options('/{routes:.*}', function (Request $request, Response $response) {
@@ -22,8 +19,19 @@ return function (App $app) {
         });
     }
 
-    $app->group('/users', function (Group $group) {
-        $group->get('', ListUsersAction::class);
-        $group->get('/{id}', ViewUserAction::class);
-    });
+    foreach (['/contestant', '/contestant/benchmark_jobs', '/contestant/benchmark_jobs/{id}', '/contestant/clarifications'] as $path) {
+        $app->get($path, function(Request $request, Response $response) {
+            $content = file_get_contents(realpath(__DIR__ . '/../public/contestant.html'));
+            $response->getBody()->write($content);
+            return $response;
+        });
+    }
+
+    foreach (['/admin', '/admin/', '/admin/clarifications', '/admin/clarifications/{id}'] as $path) {
+        $app->get($path, function(Request $request, Response $response) {
+            $content = file_get_contents(realpath(__DIR__ . '/../public/admin.html'));
+            $response->getBody()->write($content);
+            return $response;
+        });
+    }
 };
