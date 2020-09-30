29bde7f3a67ee2d29bef1ab2ff6cac3b062f6f5e
Sorah Fukumori <sorah@cookpad.com>
2020-10-01 03:24:05 +0900

    packer(xsuportal): webapp/tools
    https://github.com/isucon/isucon10-final/issues/186

--- packer/files/itamae/cookbooks/xsuportal/default.rb
+++ packer/files/itamae/cookbooks/xsuportal/default.rb
@@ -15,6 +15,8 @@ include_cookbook 'xsuportal::files'
 include_cookbook 'xsuportal::db'
 
 include_cookbook 'protoc'
+include_cookbook 'xsuportal::tools'
+
 include_cookbook 'xsuportal::golang'
 include_cookbook 'xsuportal::nodejs'
 # include_cookbook 'xsuportal::perl'
