1ebda515d472226f788718c56f814e0f2439ff80
Sho Kusano <sho-kusano@space-pirates.co.jp>
2020-09-30 10:18:55 +0900

    Fix fake debug log

--- benchmarker/scenario/verify.go
+++ benchmarker/scenario/verify.go
@@ -298,7 +298,7 @@ func verifyLeaderboard(requestedAt time.Time, leaderboard *resources.Leaderboard
 	}
 
 	if !maxMarkedAt.Equal(zero) && allowedMaxTime.Add(-cacheTime).After(maxMarkedAt) {
-		AdminLogger.Printf("OLDER LEADERBOARD: \n  %s requested at\n  %s latest finish\n  %s allowed cache time\n  %s leadeboard max time\n  %s frozen time\n", requestedAt, sLatestMarkedAt, allowedMaxTime.Add(-cacheTime), maxMarkedAt, time.Now().UTC())
+		AdminLogger.Printf("OLDER LEADERBOARD: \n  %s requested at\n  %s latest finish\n  %s allowed cache time\n  %s leadeboard max time\n  %s frozen time\n  %s now time\n", requestedAt, sLatestMarkedAt, allowedMaxTime.Add(-cacheTime), maxMarkedAt, contest.ContestFreezesAt, time.Now().UTC())
 		if allowCache {
 			return errorInvalidResponse("規定より古い内容のリーダーボードが返却されています(GET /api/audience/dashboard)")
 		} else {
